789bae1046801ca5df460717f8ab935906e4e9c1
tom_glb <1154603608@qq.com>
2019-10-30 20:16:24 +0800

    智鹏控制阀

---
+++ static/js/index.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+window.onload = function() {
+	//$(".ahead").load("ahead.html", function() {
+		
+   //  var dleft = $('#Newlist').offset().left - $('.nav-column').offset().left;  
+//       $("#Newlist").addClass("active");
+//    $('.floatr').css({
+//        "left": dleft+"px",
+//  
+//    });
+//		
+//	 
+		
+	//});
+	//$(".ahead1").load("ahead1.html");
+
+	//$(".footer").load("footer.html");
+}
+
+	
+
+
+
+
+
+
+
+
+
+
+			
